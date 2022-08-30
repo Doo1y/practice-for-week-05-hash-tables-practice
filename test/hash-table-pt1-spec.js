38c1a075bf22df77214021c02fb05ad3816f9424
Samuel Park <s.park111497@gmail.com>
2022-08-30 12:32:34 -0700

    finished practice

--- test/hash-table-pt1-spec.js
+++ test/hash-table-pt1-spec.js
@@ -10,7 +10,6 @@ describe('Hash table constructor', () => {
   beforeEach(function () {
 
     hashTable = new HashTable(2);
-
   });
 
   it('has count, capacity and data properties', () => {
@@ -22,7 +21,6 @@ describe('Hash table constructor', () => {
 
     expect(hashTable.data[0]).to.equal(null);
     expect(hashTable.data[1]).to.equal(null);
-
   });
 
   it('can initialize to different sizes', () => {
@@ -38,9 +36,7 @@ describe('Hash table constructor', () => {
     expect(hashTable.data[1]).to.equal(null);
     expect(hashTable.data[2]).to.equal(null);
     expect(hashTable.data[3]).to.equal(null);
-
   });
-
 });
 
 
@@ -51,7 +47,6 @@ describe('Hash table methods', () => {
   beforeEach(function () {
 
     hashTable = new HashTable(2);
-
   });
 
   it('hash method returns first 8 characters of sha256 hash as an integer', () => {
@@ -70,7 +65,6 @@ describe('Hash table methods', () => {
 
     // First 8 digits of sha256("hello world") are 0xb94d27b9
     expect(hashTable.hash("hello world")).to.equal(3108841401);
-
   });
 
   it('hashMod method returns the hash value modulo the number of buckets', () => {
@@ -91,7 +85,5 @@ describe('Hash table methods', () => {
     expect(hashTable.hashMod("ABC")).to.equal(4);
 
     expect(hashTable.hashMod("hello world")).to.equal(1);
-
   });
-
 });
